0ced58d7f7bdd5f810e324185375816fb701e7c8
Aminata Fall <aminatafaal03@gmail.com>
2025-07-08 12:51:15 +0000

    Trust proxy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-app.set("trust proxy", 1); 
+app.set("trust proxy", 1)
 const mongoose = require("mongoose")
 const cors = require("cors")
 const helmet = require("helmet")
